73af88fec0caa02010a8d0ba21695a3d99ad1080
Manuel Osuna <ma.osuna23@info.uas.edu.mx>
2025-05-22 12:59:46 -0700

    chore: se agrega archivo .env a git ignore con al uri de mongoDB

--- src/database.js
+++ src/database.js
@@ -1,10 +1,8 @@
 // importar mongoose para conectarnos a MongoDB
 const mongoose = require('mongoose');
-
-// importamos la configuracion de las bases de datos desde el archivo keys.js
-const { mongodb} = require('./keys');
+require('dotenv').config(); // para cargar las variables de entorno desde el archivo .env
 
 // establecemos la conexion con la base de datos
-mongoose.connect(mongodb.URI, {})
+mongoose.connect(process.env.MONGODB_URI, {})
     .then(db => console.log('Base de datos conectada')) // mensaje de exito si la conexion es exitosa
     .catch(err => console.error(err)); // captura y muestra cualquier error de conexion
--- src/keys.js
+++
@@ -1,6 +0,0 @@
-// modulo de conexion de la base de datos en la nube
-module.exports = {
-    mongodb: {
-        URI: 'mongodb+srv://Admin:Camara22@clusteruas.1atvi.mongodb.net/RedSin'
-    }
-}
